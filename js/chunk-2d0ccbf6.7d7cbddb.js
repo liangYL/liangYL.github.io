370d8af0d24c8a52d35b6d6457ff093b7e623626
liangyl <757228500@qq.com>
2022-06-02 10:12:45 +0800

    update init

--- js/chunk-2d0ccbf6.7d7cbddb.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0ccbf6"],{"4ef5":function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("h1",[e._v("这是设置页面")])},u=[],a={name:"Setting"},r=a,s=t("2877"),i=Object(s["a"])(r,c,u,!1,null,"c1793b6e",null);n["default"]=i.exports}}]);
-//# sourceMappingURL=chunk-2d0ccbf6.7d7cbddb.js.map
